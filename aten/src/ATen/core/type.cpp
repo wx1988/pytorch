a9d00780f94b5fdcb81ea101cf33351a3efe8724
Basil Hosmer <bhosmer@fb.com>
2019-09-20 09:07:03 -0700

    Typevar matching fix + implicit conversions from Scalar to int/float
    Previously, schema matching would incorrectly widen typevar bindings
    when later occurrences were supertypes of earlier ones. This allowed
    callsites like `floatlist.append(tensor.item())` to pass the typechecker,
    causing a runtime assert (issue #24856).
    
    An earlier, reverted fix (#25136) insisted on strict equality across all
    occurrences of a typevar, necessitating explicit casts around Scalar-typed
    arguments to int- or float-typed parameters, like `tensor.item()` above.
    This was the original intent, but turned out to break existing user code
    that relied on the de facto dynamic downcast. (The error only occurs when
    a specialized list representation is involved.)
    
    The current fix includes the prevention of typevar widening, but
    adds implicit conversions from Scalar to float or int as needed
    to satisfy a matched schema.
    
    TODO: dynamic resolution for ambiguous cases.
    TODO: remove temp code that preserves old behavior for special case
    
    ghstack-source-id: b0c0671e87873186fdfc3f08ad875c974a769c8a
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/26453

--- aten/src/ATen/core/type.cpp
+++ aten/src/ATen/core/type.cpp
@@ -351,7 +351,17 @@ MatchTypeReturn matchTypeVariables(
       type_env[vt->name()] = actual;
       return MatchTypeReturn::Success();
     } else if (auto unified = unifyTypes(it->second, actual)) {
-      type_env[vt->name()] = *unified;
+      // TODO: remove once tests that rely on old behavior have been fixed
+      if (it->second->isSubtypeOf(StringType::get())) {
+        if (auto opt_actual = actual->cast<OptionalType>()) {
+          if (opt_actual->getElementType()->isSubtypeOf(StringType::get())) {
+            type_env[vt->name()] = *unified;
+          }
+        }
+      }
+      // note: unifyTypes allows subtyping in either direction, so actual
+      // may be a supertype of the current binding. we're not responsible
+      // for reporting the error, only for keeping type_env stable
       return MatchTypeReturn::Success();
     }
     std::stringstream ss;
